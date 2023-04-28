40e1e1ea3a63c7187e1f89d2e818029af930d9d1
Ford Pickert <fordpickert@gmail.com>
2023-04-28 17:19:32 -0500

    trying different ways of serializing header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,35 @@
 import { useState, useEffect } from 'react';
-import { getPosts } from '../../utils';
+import { GetServerSideProps } from 'next';
+import { allPostsURL } from '../../utils';
+import axios from 'axios';
 import StdHome from '@/components/StdHome';
 import Nav from '@/components/Nav';
 
 // Make hooks for pulling all info from db, serverSideProps, then pass where needed
 
-export default function Home() {
+export default function Home({ allPosts }) {
 	// STATE
-	const [posts, setPosts] = useState({});
 	const [loading, setLoading] = useState(false);
 	const [loggedIn, setLoggedIn] = useState(false);
-
-	useEffect(() => {
-		const allPosts = getPosts();
-		setPosts(allPosts);
-	}, []);
 
 	return (
 		<>
 			<div className='border-2 border-red-500 min-h-screen mx-auto'>
 				<Nav />
 				<div>
-					<StdHome posts={posts} />
+					<StdHome posts={allPosts} />
 					{/* Content goes here, conditionally rendered based on logged in or not */}
 				</div>
 			</div>
 		</>
 	);
+}
+
+export async function getServerSideProps() {
+	const allPosts = await axios.get(allPostsURL);
+	console.log(allPosts);
+
+	return {
+		props: { allPosts: allPosts || null },
+	};
 }
--- utils.ts
+++ utils.ts
@@ -1,17 +1,4 @@
 import axios from 'axios';
 
 // API Endpoints (Local)
-const allPostsURL = 'http://localhost:4000/posts';
-
-// Display all current posts on homepage, regardless of login status
-export const getPosts = async () => {
-	try {
-		const posts = await axios.get(allPostsURL);
-		console.log(posts);
-		return posts;
-	} catch (err) {
-		console.error(err);
-	}
-};
-
-// Get user info
+export const allPostsURL = 'http://localhost:4000/posts';
