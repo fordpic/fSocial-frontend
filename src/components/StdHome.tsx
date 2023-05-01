98c391a59dc3b64c57dc29aec689baef1eb77203
Ford Pickert <fordpickert@gmail.com>
2023-05-01 14:09:24 -0500

    posts loading correctly from db

--- src/components/StdHome.tsx
+++ src/components/StdHome.tsx
@@ -1,15 +1,16 @@
+// Likely deleting, found better way
 export default function StdHome({ posts }) {
 	return (
 		<div className='flex justify-center items-center h-screen w-full border-2 border-green-500'>
 			<div className='border border-purple-500 w-[75%] h-[70%] text-center justify-center'>
 				<div>
 					{posts &&
-						posts?.data?.map((post: any) => {
-							<div key={post.id}>
+						posts?.data?.posts?.map((post: any) => (
+							<div key={post?.id}>
 								<h1 className='text-black'>{post?.title}</h1>
 								<p>{post?.content}</p>
-							</div>;
-						})}
+							</div>
+						))}
 				</div>
 			</div>
 		</div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,16 +9,18 @@ import Nav from '@/components/Nav';
 export default function Home() {
 	// STATE
 	const [posts, setPosts] = useState({});
-	const [loading, setLoading] = useState(true);
 	const [loggedIn, setLoggedIn] = useState(false);
+	const [loaded, setLoaded] = useState(false);
 
 	useEffect(() => {
-		axios.get(allPostsURL).then((res) => setPosts(res));
+		axios.get(allPostsURL).then((res) => {
+			setPosts(res);
+			setLoaded(true);
+		});
 		console.log(posts);
-		setLoading(false);
 	}, []);
 
-	if (loading) {
+	if (!loaded) {
 		return null;
 	} else {
 		return (
@@ -26,7 +28,13 @@ export default function Home() {
 				<div className='border-2 border-red-500 min-h-screen mx-auto'>
 					<Nav />
 					<div>
-						<StdHome posts={posts} />
+						{/* @ts-ignore */}
+						{posts?.data?.map((post: any) => (
+							<div key={post?.id}>
+								<h1>{post?.title}</h1>
+								<h1>{post?.content}</h1>
+							</div>
+						))}
 						{/* Content goes here, conditionally rendered based on logged in or not */}
 					</div>
 				</div>
