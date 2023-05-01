8ccc09ed6b34fe08126d1d1428db0a7fdf7eba75
Ford Pickert <fordpickert@gmail.com>
2023-05-01 14:45:12 -0500

    ide bugging, fixes

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,6 +1,6 @@
 export default function PostCard({ post }) {
 	return (
-		<div className='text-center p-3 mt-12 mx-24 space-y-2 bg-slate-400/60 border border-purple-300 shadow-md'>
+		<div className='text-center p-3 mt-12 mx-24 space-y-2 bg-slate-400/60 hover:bg-slate-400/50 border border-purple-300 shadow-md'>
 			<h1 className='text-purple-400 text-md font-semibold'>{post?.title}</h1>
 			<h1 className='text-md'>{post?.author}</h1>
 			<p className='text-sm text-ellipsis'>{post?.content}</p>
